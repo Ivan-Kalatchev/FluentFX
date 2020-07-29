52afa906a602b4e90656be189b221a6cab307f3a
TPAD\Ivan <mani.munana@gmail.com>
2020-07-30 01:24:46 +0300

    neshta

--- demo-maven/demo/src/main/java/demo/PrimaryController.java
+++ demo-maven/demo/src/main/java/demo/PrimaryController.java
@@ -1,14 +1,28 @@
 package demo;
 
 import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.*;
+
+import org.controlsfx.glyphfont.FontAwesome;
+import org.controlsfx.glyphfont.Glyph;
+import org.controlsfx.glyphfont.GlyphFont;
+import org.controlsfx.glyphfont.GlyphFontRegistry;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 public class PrimaryController {
 
     @FXML
     public TextField AzNeZnam;
+
+    @FXML
+    public ImageView Ham;
+
+    @FXML
+    public Button Ham1;
 
     @FXML
     public TreeView<TreeItem<String>> NavigationView;
@@ -20,23 +34,31 @@ public class PrimaryController {
 
     @FXML
     private void WriteNE() throws IOException {
-        System.out.println(AzNeZnam.getText());
-        AzNeZnam.setText("NZZZZZZZZZZZZZ");
+        //System.out.println(AzNeZnam.getText());
+        //AzNeZnam.setText("NZZZZZZZZZZZZZ");
 
         TreeItem rootItem = new TreeItem("Tutorials");
 
-        TreeItem webItem = new TreeItem("Web Tutorials");
-        webItem.getChildren().add(new TreeItem("HTML  Tutorial"));
-        webItem.getChildren().add(new TreeItem("HTML5 Tutorial"));
-        webItem.getChildren().add(new TreeItem("CSS Tutorial"));
-        webItem.getChildren().add(new TreeItem("SVG Tutorial"));
+        GlyphFont fontAwesome = GlyphFontRegistry.font("FontAwesome");
+
+        FontIcon aIcon = new FontIcon();
+
+        Ham1.setGraphic(fontAwesome.create(FontAwesome.Glyph.BARS));
+
+        TreeItem webItem = new TreeItem("Data");
+        webItem.setGraphic(fontAwesome.create(FontAwesome.Glyph.TABLE));
         rootItem.getChildren().add(webItem);
 
-        TreeItem javaItem = new TreeItem("Java Tutorials");
-        javaItem.getChildren().add(new TreeItem("Java Language"));
-        javaItem.getChildren().add(new TreeItem("Java Collections"));
-        javaItem.getChildren().add(new TreeItem("Java Concurrency"));
+        TreeItem aItem = new TreeItem("Camera");
+        aItem.setGraphic(fontAwesome.create(FontAwesome.Glyph.CAMERA));
+
+        TreeItem javaItem = new TreeItem("Buttons");
+        javaItem.setGraphic(fontAwesome.create(FontAwesome.Glyph.BUILDING));
+
         rootItem.getChildren().add(javaItem);
+        rootItem.getChildren().add(aItem);
+        System.out.println("Raboti");
         NavigationView.setRoot(rootItem);
+        System.out.println("Raboti2");
     }
 }
--- demo-maven/demo/src/main/java/module-info.java
+++ demo-maven/demo/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.materialdesign;
 
     opens demo to javafx.fxml;
     exports demo;
